746bc24db3bd27a35f54003cc2d6238ef96cfdb6
Himanshu Singh <himanshusingh48415@gmail.com>
2025-01-02 17:22:52 +0530

    Fix tailwind.config.js for ESM compatibility

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from "tailwindcss-animate";
 export const darkMode = ["class"];
 export const content = [
 	'./pages/**/*.{js,jsx}',
@@ -72,4 +73,4 @@ export const theme = {
 		},
 	},
 };
-export const plugins = [require("tailwindcss-animate")];
+export const plugins =  [tailwindcssAnimate]
