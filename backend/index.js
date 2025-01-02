adcd2e00c8502b574a39a4bd3187fc98be77beca
Himanshu Singh <himanshusingh48415@gmail.com>
2025-01-02 22:41:55 +0530

    updated urls

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOption = {
-    origin : 'http://localhost:5173',
+    origin : "https://jobportal-fullstack-g9he.onrender.com",
     credentials : true
 }
 app.use(cors(corsOption));
